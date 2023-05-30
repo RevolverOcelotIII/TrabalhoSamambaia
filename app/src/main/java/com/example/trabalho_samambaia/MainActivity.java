76c7bcde8f8a221beea17376051667f6a11c1899
Jeoston Araújo <jeostonjunior@gmail.com>
2023-05-29 21:47:26 -0300

    [Feat] - Creating the splash screen in a new activity

--- app/src/main/java/com/example/trabalho_samambaia/MainActivity.java
+++ app/src/main/java/com/example/trabalho_samambaia/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.trabalho_samambaia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.splash_background_color));
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, HomePage.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
