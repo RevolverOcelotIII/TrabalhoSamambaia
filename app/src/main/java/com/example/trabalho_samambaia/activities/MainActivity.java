eb2b7a03092517e0e8ad26dcb4b685d9ec9a97bd
Eduardo Correia <correiaeduardojr@gmail.com>
2023-06-30 09:03:22 -0300

    feat: add firebase analytics integration

--- app/src/main/java/com/example/trabalho_samambaia/activities/MainActivity.java
+++ app/src/main/java/com/example/trabalho_samambaia/activities/MainActivity.java
@@ -8,8 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.trabalho_samambaia.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     BottomNavigationView bottomNavigationView;
 
@@ -38,5 +42,16 @@ public class MainActivity extends AppCompatActivity {
 //        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 //
 //        getSupportFragmentManager().beginTransaction().replace(R.id.container, home).commit();
+
+        setupFirebase();
+    }
+
+    private void setupFirebase() {
+        FirebaseApp.initializeApp(this);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.METHOD, "onCreate");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
     }
 }
